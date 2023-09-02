377eeded42365c38ca5f0ecebcce92397893fa75
Santimore <santyagomoreyra@gmail.com>
2023-09-02 16:41:04 -0300

    segunda

--- AppViajes/urls.py
+++ AppViajes/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from AppViajes import views
 
 urlpatterns = [
-    path('AppViajes/',include('AppViajes.urls')),
+
 ]
--- AppViajes/views.py
+++ AppViajes/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from AppViajes.models import *
 # Create your views here.
+
+def inicio (request):
+    return render
